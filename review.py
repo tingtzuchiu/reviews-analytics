bd32d89517e7bef039067baaf397a51ed72f65d7
Claire Chiu <tingtzuchiu@gmail.com>
2019-07-12 15:26:29 -0700

    add world count

--- review.py
+++ review.py
@@ -7,9 +7,6 @@ with open("reviews.txt","r") as f:
 		if count % 1000 == 0:
 			print(len(data))
 print("檔案讀取完了，總共有",len(data),"筆資料")
-#print(data)
-#print(data[0])
-
 
 sum_len = 0
 for d in data:
@@ -23,3 +20,47 @@ for d in data:
 print("一共有",len(new),"筆留言長度小於100")
 print(new[0])
 print(new[1])
+
+good = []
+for d in data:
+	if "good" in d:
+		good.append(d)
+print("一共有",len(good),"筆留言提到good")
+
+# good = [d for d in data if "good" in d]
+# print(good)
+
+# bad =["bad" in d for d in data]
+
+# bad = []
+# for d in data:
+# 	bad.append("bad" in d)
+
+# print(bad)
+
+#文字計數
+wc = {} #word count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+print(len(wc))
+print(wc["Claire"])
+
+while True:
+	word = input("請問你想查什麼字：")
+	if word == "q":
+		break
+	if word in wc:
+		print(word, "出現過的次數：", wc[word])
+	else:
+		print("這個字沒有出現過喔！")
+print("感謝使用本查詢功能")
